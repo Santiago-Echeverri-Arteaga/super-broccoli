caa8af1da9a19edee73157cc644e9b24a66bc931
Santiago Echeverri Arteaga <secheverri@uniquindio.edu.co>
2022-04-25 21:34:07 -0500

    Arreglo metodo get

--- app/api/api_v1/endpoints/heroes.py
+++ app/api/api_v1/endpoints/heroes.py
@@ -57,7 +57,7 @@ async def create_heroe():
 async def review_heroe(nombre: str): 
     sql = f"""
         SELECT name FROM Heroes WHERE
-        name = {nombre}
+        name = '{nombre}'
         """
     response = rds_client.execute_statement(
        secretArn= db_credentials_secrets_store_arn,
